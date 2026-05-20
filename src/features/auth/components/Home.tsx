d2feabb49dcbd1cc2824479d6b3dac56d522f15f
Florian <j.flo83@laposte.net>
2026-05-20 09:30:06 +0200

    update image

--- src/features/auth/components/Home.tsx
+++ src/features/auth/components/Home.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import HeaderUnsigned from "../../../components/HeaderUnsigned";
 import Footer from "../../../components/Footer";
 import Image from "next/image";
-import welcomeImage from "../../../../public/simsponAccueil1.webp";
-import spiderCochon from "../../../../public/spiderCochon1.webp";
 
 function Home() {
   return (
@@ -12,15 +10,23 @@ function Home() {
 
       <main className="h-(--size-screen-content)">
         <div className="flex justify-end">
-          <Image src={spiderCochon} alt="SpiderCochon" />
-        </div>
-        <div className=" flex flex-col items-center">
           <Image
-            src={welcomeImage}
-            alt="WelcomeImage"
-            height={573}
-            width={539}
+            src="/spiderCochon.webp"
+            width={110}
+            height={90}
+            alt="SpiderCochon"
           />
+        </div>
+        <div className="flex flex-col items-center justify-center">
+          <div className="">
+            <Image
+              src="/simsponAccueil1.webp"
+              alt="WelcomeImage"
+              height={573}
+              width={539}
+            />
+          </div>
+
           <div className="flex flex-col gap-7">
             <button className="bg-simpson-orange rounded-md text-white px-5 h-10 cursor-pointer text-medium">
               Se connecter
