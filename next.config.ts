1edd561e686f41ed2f1483d5e85c193f25964909
lnamur <lnamur@pm.me>
2026-05-27 16:49:19 +0200

    suppress warning next image

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images:{
-    domains: ["cdn.thesimpsonsapi.com"],
-  },
-  typescript: {
-    ignoreBuildErrors: true,      
-  } ,
   /* config options here */
   reactCompiler: true,
 };
